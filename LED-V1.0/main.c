591d6d7b3fdc12369857e1bd5aec3fed7c335927
AalaaIbrahim <118854389+AalaaIbrahim@users.noreply.github.com>
2023-06-16 22:56:34 +0300

    create: project folders

---
+++ LED-V1.0/main.c
@@ -0,0 +1,20 @@
+/**
+ ** @file      main.c
+ ** @brief     
+ ** @author    
+ ** @date      June 16, 2023
+ ** @version   0.1
+ */
+
+/*- INCLUDES
+----------------------------------------------*/
+#include "app.h"
+
+void main(void)
+{
+	
+	while(1)
+	{
+		
+	}
+}
